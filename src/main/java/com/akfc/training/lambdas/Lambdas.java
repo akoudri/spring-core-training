1eda0082ae50f5750010b32f35326ea4977e6674
akoudri <akoudri@cenotelie.fr>
2023-12-12 18:51:14 +0100

    Adds exercises on lambdas

---
+++ src/main/java/com/akfc/training/lambdas/Lambdas.java
@@ -0,0 +1,7 @@
+package com.akfc.training.lambdas;
+
+public class Lambdas {
+    public static void main(String[] args) {
+        //TODO: compose x*x and x+5
+    }
+}
---
+++ src/main/java/com/akfc/training/lambdas/Sex.java
@@ -0,0 +1,5 @@
+package com.akfc.training.lambdas;
+
+public enum Sex {
+    MALE, FEMALE;
+}
